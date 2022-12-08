cf42428c7c3115787a9a82556c47a1a2a60352cb
Nessy01 <72563733+Nessy01@users.noreply.github.com>
2022-12-08 18:00:51 +0100

    added plugins

--- app/src/main/java/com/example/sqlite/MainActivity.kt
+++ app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.sqlite
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
